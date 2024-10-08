0691621c3d3297c830d53de4012f60bdf6dc2ed0
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-10-08 23:04:41 +0700

    refactor: add navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-bg dark:bg-black text-[#171717] dark:text-foreground`}
       >
-        {children}
+      <Navbar />
+      {children}
       </body>
     </html>
   );
