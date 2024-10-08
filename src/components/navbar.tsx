347721386ed4bc9a9ece5807ada9efa6960e5220
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-10-08 23:03:53 +0700

    feat: create navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import ButtonMode from "@/components/btnMode";
+
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between w-full px-12 py-4 sticky top-0">
+            <Link href="/" className="text-2xl font-bold">
+                Joki <span className="text-red-700">Tugas</span>
+            </Link>
+
+            <div className="flex gap-4">
+                <Link href="/" className="text-lg">
+                    Home
+                </Link>
+                <Link href="/about" className="text-lg">
+                    Cara Order
+                </Link>
+                <Link href="/contact" className="text-lg">
+                    Testimonial
+                </Link>
+                <Link href="/blog" className="text-lg">
+                    Blog
+                </Link>
+            </div>
+
+            <ButtonMode />
+        </nav>
+    )
+}
