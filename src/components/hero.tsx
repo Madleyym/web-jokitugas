13805594d4ec8add8bdf8136f98056145c153517
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-09 13:45:12 +0700

    refactor: change style of Heading

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,8 +4,8 @@ export default function Hero() {
     return(
         <div className="flex px-12 items-center">
             <div className="flex flex-col gap-4 w-1/2">
-                <p className="text-red-700">#jasajokitugas</p>
-                <h1 className="text-6xl font-bold">Joki Tugas</h1>
+                <p className="text-fg">#jasajokitugas</p>
+                <h1 className="text-6xl font-bold">Joki <span className="text-fg">Tugas</span></h1>
                 <h1 className="text-6xl font-bold">Murah & Cepat</h1>
                 <div>
                     <p><b className="font-bold">Jasa Pengerjaan Tugas</b> paling murah dan cepat di Indonesia.</p>
