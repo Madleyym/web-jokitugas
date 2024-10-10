6ec548178ce12832509fff44381a47e151fe0209
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-10 20:33:49 +0700

    feat: Add MetodeBayar component

---
+++ src/components/metodeBayar.tsx
@@ -0,0 +1,12 @@
+import ShortText from "@/components/shortText";
+
+export default function MetodeBayar() {
+    return (
+        <div>
+            <ShortText
+                title="Metode Pembayaran"
+                desc="Kamu masih bingung metode pembayaran apa saja yang dapat digunakan saat order joki tugas di Buatin Tugasku? Jangan khawatir, ada banyak metode pembayaran kok! Pembayaran dapat dilakukan melalui Transfer Bank (BCA dan BRI) dan E-Wallet (Dana, Gopay, Ovo, Shopeepay, dan Linkaja)"
+            />
+        </div>
+    )
+}
