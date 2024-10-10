a9547355e216a14bf1fa8ab57261cf9a62141743
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-10 20:36:30 +0700

    feat: Add Card component for displaying information

---
+++ src/components/card.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Card({ image, title, description }: { image: string, title: string, description: string }) {
+    return (
+        <div
+            className="mb-4 flex flex-col justify-between gap-2 max-w-72 p-4 border border-fg rounded-xl transform scale-100 hover:scale-105 transition-transform duration-300">
+            <div className="flex items-end h-full">
+                <Image src={image} alt="at_work" width={200} height={200} className="mx-auto"/>
+            </div>
+            <div>
+                <h1 className="font-bold text-fg">{title}</h1>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
