57609c891cf63cc8febd81df19dbaae513910b92
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-10 20:35:32 +0700

    chore: Update Features component styling to use smaller padding

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -2,7 +2,7 @@ import {HoverEffect} from "@/components/ui/card-hover-effect";
 
 export default function Features() {
     return (
-        <div className="max-w-5xl mx-auto px-8">
+        <div className="px-12">
             <HoverEffect items={features} />
         </div>
     )
