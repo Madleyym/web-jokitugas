f9cafbdc160edd0463adb5d6a732ed2f33bf13c5
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-10 20:37:57 +0700

    feat: Add lazy loading for images in Daftar Tugas page

--- src/app/daftar-tugas/page.tsx
+++ src/app/daftar-tugas/page.tsx
@@ -1,31 +1,37 @@
 "use client";
 
 import React from "react";
-import {Boxes} from "@/components/ui/background-boxes";
-import {cn} from "@/lib/utils";
+import {CardTugas} from "@/components/cardTugas";
+import Hero2 from "@/components/hero2";
 
 export default function page() {
     return (
         <div>
-            <div
-                className="h-80 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
-                <div
-                    className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none"/>
-
-                <Boxes/>
-                <h1 className={cn("md:text-6xl text-xl text-white relative z-20")}>
-                    Daftar Tugas
-                </h1>
-                <p className="text-center mt-2 text-neutral-300 relative z-20">
-                    Tugas Apapun Serba Bisa
-                </p>
-            </div>
+            <Hero2 title="Daftar Tugas" txt="Tugas Apapun Serba bisa" />
 
             <div className="max-w-5xl text-center mx-auto mt-12">
                 <h2 className="text-4xl font-bold text-fg">Daftar Tugas Unggulan Kami</h2>
                 <p className="mt-2">
                     Berikut ini adalah tugas unggulan dan paling diminati oleh pelanggan Kami selama beberapa bulan terakhir.
                 </p>
+            </div>
+
+            <div className="flex gap-8 justify-center">
+                <CardTugas
+                    title="Tugas Makalah"
+                    description="Makalah adalah karya tulis pelajar atau mahasiswa sebagai laporan hasil pelaksanaan tugas sekolah atau perguruan tinggi."
+                    image="/homework.png"
+                />
+                <CardTugas
+                    title="Tugas Esai"
+                    description="Esai adalah karangan prosa yang membahas suatu masalah secara sepintas lalu dari sudut pandang penulis."
+                    image="/webinar-amico.svg"
+                />
+                <CardTugas
+                    title="Tugas Presentasi"
+                    description="Presentasi adalah kegiatan menyampaikan informasi, gagasan, atau laporan kepada orang lain."
+                    image="/analytics.svg"
+                />
             </div>
         </div>
     );
