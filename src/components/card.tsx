66cdea47b598a34415fbb3adfa3c53e0b77a6dea
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-11 18:55:23 +0700

    refactor: Update Card component styling and center align image

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Card({ image, title, description }: { image: string, title: string, description: string }) {
     return (
         <div
-            className="mb-4 flex flex-col justify-between gap-2 max-w-64 p-4 border border-fg rounded-xl transform scale-100 hover:scale-105 transition-transform duration-300">
+            className="mb-4 flex flex-col justify-between mx-auto gap-2 max-w-64 p-4 border border-fg rounded-xl transform scale-100 hover:scale-105 transition-transform duration-300">
             <div className="flex items-end h-full">
                 <Image src={image} alt="at_work" width={200} height={200} className="mx-auto"/>
             </div>
