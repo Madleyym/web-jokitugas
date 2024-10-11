9e1541be77b93f7b6db4e2ee853ebe43974a74c9
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-11 18:55:30 +0700

    refactor: Update BtnPrimary component to include dynamic href prop

--- src/components/btnPrimary.tsx
+++ src/components/btnPrimary.tsx
@@ -1,9 +1,9 @@
 import {Icon} from "@iconify/react";
 import Link from "next/link";
 
-export default function BtnPrimary ({txt, classname} : {txt: string, classname?: string}) {
+export default function BtnPrimary ({txt, classname, href} : {txt: string, classname?: string, href: string}) {
     return (
-        <Link href="/daftar-tugas" className={`${classname} bg-fg py-2 px-4 rounded-full flex items-center gap-2 text-white hover:bg-transparent border border-transparent hover:border-fg hover:text-fg`}>
+        <Link href={href} className={`${classname} bg-fg py-2 px-4 rounded-full flex items-center gap-2 text-white hover:bg-transparent border border-transparent hover:border-fg hover:text-fg`}>
             {txt}
             <Icon icon="mingcute:right-line" />
         </Link>
