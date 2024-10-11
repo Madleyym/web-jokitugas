bf86a48021e6394a16686a7489fbf6ec10a6c27f
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-11 18:54:53 +0700

    refactor: Update Hero component layout and content styling

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,17 +3,17 @@ import BtnPrimary from "@/components/btnPrimary";
 
 export default function Hero() {
     return(
-        <div className="flex px-12 items-center">
-            <div className="flex flex-col gap-4 w-2/3">
+        <div className="flex flex-col-reverse lg:flex-row lg:px-12 items-center">
+            <div className="flex flex-col gap-4 lg:w-2/3">
                 <p className="text-fg">#jasajokitugas</p>
-                <h1 className="text-6xl font-bold">Joki <span className="text-fg">Tugas</span></h1>
-                <h1 className="text-6xl font-bold">Murah & Cepat</h1>
+                <h1 className="text-3xl lg:text-6xl font-bold">Joki <span className="text-fg">Tugas</span></h1>
+                <h1 className="text-3xl lg:text-6xl font-bold">Murah & Cepat</h1>
                 <div>
                     <p><b className="font-bold">Jasa Pengerjaan Tugas</b> paling murah dan cepat di Indonesia.</p>
                     <p>Melayani <b className="font-bold">pembuatan tugas</b> mulai dari sekolah s/d kuliah.
                     </p>
                 </div>
-                <BtnPrimary txt="Konsultasikan Sekarang" classname="w-max" />
+                <BtnPrimary href="https://whatsapp" txt="Konsultasikan Sekarang" classname="w-max" />
             </div>
 
             <Image src="/hero-banner.png" alt="banner-home" width={1000} height={1000}/>
