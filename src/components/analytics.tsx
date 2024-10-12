e9ec682dcfb6b02e4f7c1713589c68866907b0a0
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-10-12 13:33:33 +0700

    refactor: change style sizr for more responsive in every device

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -2,19 +2,19 @@ import {Icon} from "@iconify/react";
 
 export const Card = ({icon, title, detail}: { icon: string, title: string, detail: string }) => {
     return (
-        <div className="text-center flex flex-col p-4 text-white">
-            <div className="text-3xl lg:text-6xl font-bold w-full text-center">
+        <div className="text-center flex flex-col text-white">
+            <div className="text-2xl lg:text-6xl font-bold w-full text-center">
                 <Icon icon={icon} className="mx-auto -mb-8 text-zinc-100/45"/>
                 <h4>{title}</h4>
             </div>
-            <p>{detail}</p>
+            <p className="text-sm">{detail}</p>
         </div>
     )
 }
 
 export default function Analytics() {
     return (
-        <div className="bg-fg flex rounded-2xl my-20">
+        <div className="bg-fg flex gap-4 p-4 mx-4 rounded-2xl my-20">
             <Card icon="mdi:account-student" title="500" detail="Total Pelanggan"/>
             <Card icon="basil:book-open-solid" title="1200" detail="Tugas Diselesaikan"/>
             <Card icon="fa-solid:university" title="50" detail="Universitas Berbeda"/>
