7d599c21f34c6857192143b9ad9819f10574a7dd
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-16 21:25:35 +0700

    refactor: add wa floating button into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import FloatingChatButton from "@/components/ui/floating-chat-button";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
       <Navbar />
       {children}
+      <FloatingChatButton />
       <Footer />
       </body>
     </html>
