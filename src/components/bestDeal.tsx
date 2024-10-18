168a6a7f52b2de6abd059ce5d13b20daec5ac885
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-18 23:41:51 +0700

    refactor: Update BestDeal component content and styling

--- src/components/bestDeal.tsx
+++ src/components/bestDeal.tsx
@@ -9,11 +9,14 @@ export default function BestDeal() {
             <div className="flex flex-col gap-4">
                 <h1 className="text-6xl font-bold">Harga Mulai <span className="text-fg">50 ribuan!</span></h1>
                 <p>
-                    <span className="text-fg font-semibold">Joki Tugas</span> menyediakan layanan jasa joki pengerjaan tugas dengan biaya yang murah dan terjangkau di kantong pelajar dan mahasiswa. Biaya pengerjaan tugas dimulai dengan harga 50 ribu saja.
+                    <span className="text-fg font-semibold">
+                        joki-tugasku.com{" "}
+                    </span>
+                    menyediakan jasa pengerjaan tugas dengan harga yang ramah di kantong pelajar dan mahasiswa. Mulai dari 50 ribu saja, tugas-tugasmu akan selesai tanpa ribet.
                 </p>
                 <hr/>
                 <p>
-                    Masa ngajak keluar doi setiap malam minggu dan bayarin semua kebutuhannya bisa, tapi bayar untuk kebutuhan diri sendiri supaya dapet nilai bagus nggak bisa? Yuk cek harga tugasmu sekarang!
+                    Masa nongkrong bareng teman dan traktir doi tiap akhir pekan bisa, tapi investasi buat nilai bagus masih mikir-mikir? Yuk, cek harga jasa kami dan biarkan kami bantu menyelesaikan tugasmu sekarang!
                 </p>
                 <Link href="/src/components/cardTugas" className="w-max bg-fg py-2 px-4 rounded-full flex items-center gap-2 text-white hover:bg-transparent border border-transparent hover:border-fg hover:text-fg">
                     Cek Harga Tugasmu
